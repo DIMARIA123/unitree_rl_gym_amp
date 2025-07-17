30e1063e4dbf8c7bbb527671eddd6a335f61e807
kevin <593485200@qq.com>
2025-07-17 16:54:48 +0800

    feat:add third libs

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(name='unitree_rl_gym',
       version='1.0.0',
       author='Unitree Robotics',
       license="BSD-3-Clause",
-      packages=find_packages(),
+      packages=find_packages(where="utils"),         # 在 utils 目录下查找所有子包
+      package_dir={"": "utils"},                     # 告诉 pip 包的根路径是 utils/
       author_email='support@unitree.com',
       description='Template RL environments for Unitree Robots',
-      install_requires=['isaacgym', 'rsl-rl', 'matplotlib', 'numpy==1.20', 'tensorboard', 'mujoco==3.2.3', 'pyyaml'])
+      install_requires=['isaacgym', 'rsl-rl', 'matplotlib', 'numpy==1.24', 'tensorboard', 'mujoco==3.2.3', 'pyyaml'])

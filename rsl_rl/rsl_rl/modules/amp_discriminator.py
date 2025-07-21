b521a010e86e5746fe6146c83d278142868a411f
kevin <593485200@qq.com>
2025-07-21 19:37:43 +0800

    feat: add compute_grad_pen

--- rsl_rl/rsl_rl/modules/amp_discriminator.py
+++ rsl_rl/rsl_rl/modules/amp_discriminator.py
@@ -30,6 +30,23 @@ class AMPDiscriminator(nn.Module):
         logits = self.amp_linear(x)
         return logits
     
+    def compute_grad_pen(self,
+                        expert_state,
+                        expert_next_state,
+                        lambda_=10):
+        expert_data = torch.cat([expert_state, expert_next_state], dim=-1)
+        expert_data.requires_grad = True
+
+        disc = self.amp_linear(self.trunk(expert_data))
+        ones = torch.ones(disc.size(), device=disc.device)
+        grad = autograd.grad(
+            outputs=disc, inputs=expert_data,
+            grad_outputs=ones, create_graph=True,
+            retain_graph=True, only_inputs=True)[0]
+
+        # Enforce that the grad norm approaches 0.
+        grad_pen = lambda_ * (grad.norm(2, dim=1) - 0).pow(2).mean()
+        return grad_pen
     
 # model = AMPDiscriminator(8, [16,256], 'cuda:0')
 # print(model)
